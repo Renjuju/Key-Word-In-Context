44dd4af584d114f0933fe916dd0a1e2718763b57
renju <renju467@gmail.com>
2016-01-17 18:35:58 -0500

    Circular algorithm now implemented...cleaning up code and writing unit tests for next commit probably

--- src/kwic/Kwic.java
+++ src/kwic/Kwic.java
@@ -1,6 +1,6 @@
 package kwic;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Vector;
 
 public class Kwic {
@@ -15,14 +15,23 @@ public class Kwic {
 		
 	}
 	
-	public Vector<String>kwicParser(Vector<String> input) {
+	public Vector<String> kwicParser(Vector<String> input) {
 		Vector<String> finalOutput = new Vector<String>();
 		for(int i = 0; i < input.size(); i++) {
-			String output = quickStringBuilder(input.get(i));
-			finalOutput.add(output);
+			int lineLength = getArrLength(input.get(i));
+			String output = input.get(i);
+			for(int x = 0; x < lineLength; x++) {
+				output = quickStringBuilder(output);
+				finalOutput.add(output);	
+			}
 		}
+		Collections.sort(finalOutput, String.CASE_INSENSITIVE_ORDER);
 		printInput(finalOutput);
 		return finalOutput;
+	}
+	
+	public Vector<String> kwicParser() {
+		return kwicParser(input);
 	}
 	
 	private void printInput(Vector<String> input) {
@@ -36,25 +45,30 @@ public class Kwic {
 		System.out.println("");
 	}
 	
+	private int getArrLength(String input) {
+		String[] inputAr = input.replaceAll("[^a-zA-Z ]", "").split("\\s+");
+		return inputAr.length;
+	}
 	private String quickStringBuilder(String input) {
-		String[] x = input.replaceAll("[^a-zA-Z ]", "").split("\\s+");
-		input = input.replaceAll("\\W", "");
-		String temp = x[x.length - 1];
-		x[x.length - 1] = x[0];
-		x[0] = temp;
-		String y = null;
-		for(int i = 0; i < x.length; i++) {
-			if(y == null) {
-				y = x[i];
+		String[] inputAr = input.replaceAll("[^a-zA-Z ]", "").split("\\s+");
+//		input = input.replaceAll("\\W", "");
+		String temp = inputAr[inputAr.length - 1];
+		String concatString = null;
+		for(int i = 0; i < inputAr.length; i++) {
+			if(i == 0) {
+				continue;
+			}
+			if(concatString == null) {
+				concatString = inputAr[i];
 			} else {
-				y+=" " + x[i];
+				concatString+=" " + inputAr[i];
 			}
 		}
-//		System.out.println(y);
-		return y;
+		concatString+= " " + inputAr[0];
+//		System.out.println(concatString);
+		return concatString;
 		//refactor this portion of the code
 		//the you from 8:02:28 knows exactly what TODO
 		//DO YOU?
-		//nigguh
 	}
 }
--- src/kwic/KwicRunner.java
+++ src/kwic/KwicRunner.java
@@ -4,10 +4,9 @@ import java.util.Vector;
 
 public class KwicRunner {
 	public static void main(String args[]) {
-		Kwic tooQuick = new Kwic();
 		Input userInput = new Input(); 
 		Vector<String> testVal = userInput.getUserInput();
-		tooQuick.kwicParser(testVal);
-		
+		Kwic tooQuick = new Kwic(testVal);
+		tooQuick.kwicParser();
 	}
 }
